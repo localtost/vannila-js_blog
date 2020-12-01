b9fc10dd47a106fb7195e137cebd6f57cc2d8ea9
Kumskov-dev <Artemka19021991>
2020-12-01 15:02:38 +0200

    finished navigation tabs

---
+++ src/components/favorite.js
@@ -0,0 +1,8 @@
+import Component
+    from "../core/component";
+
+export class Favorite extends Component {
+    constructor(id) {
+        super(id);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import Header from "./components/header";
+import Navigation from "./components/navigation";
+import {Create} from "./components/create";
+import {Favorite} from "./components/favorite";
+import {Posts} from "./components/posts";
 
-const header = new Header('header');
+new Header('header');
+const navigation = new Navigation('navigation')
+navigation.registerTabs([
+    {name:'create',component : new Create('create')},
+    {name:'favorite',component : new Favorite('favorite')},
+    {name:'posts',component : new Posts('posts')}
+])
