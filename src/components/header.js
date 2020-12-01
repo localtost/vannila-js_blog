1f704469e3460d351b01bf8ba07117071c781388
Kumskov-dev <Artemka19021991>
2020-12-01 13:01:08 +0200

    created component & created header component

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import Component
+    from "../core/component";
+
+export default class Header extends Component {
+    constructor(id) {
+        super(id);
+    }
+
+    init() {
+        const btn = this.$el.querySelector('.js-header-start')
+        btn.addEventListener('click', buttonHandler.bind(this))
+    }
+}
+
+function buttonHandler() {
+    this.hide();
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import Header from "./components/header";
+
+const header = new Header('header');
